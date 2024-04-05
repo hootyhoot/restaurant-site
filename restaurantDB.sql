a38ac7621744d27bcbba067f288312b75cf8a2dd
Floopert <ngwengjie@hotmail.com>
2024-04-05 23:32:14 +0800

    -ADDED SQL STATEMENTS TO ADD DEFAULT FOOD DATA -ADDED IMAGES TO BE IMPORTED FOR DEFAULT FOOD

--- restaurantDB.sql
+++ restaurantDB.sql
@@ -35,18 +35,33 @@ CREATE TABLE IF NOT EXISTS Food(
     FoodName VARCHAR(50) NOT NULL,
     Price DECIMAL(6,2) NOT NULL,
     Availability ENUM('Available', 'Not Available') NOT NULL,
-    /*FoodPic stores file path to picture only, not going to store the picture itself*/
-    FoodPic VARCHAR(255) NOT NULL, 
+    FoodPic BLOB NOT NULL,
     Description VARCHAR(255),
     CategoryID INT,
     FOREIGN KEY (CategoryID) REFERENCES Category(CategoryID) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 
+
+
 CREATE TABLE IF NOT EXISTS Category(
     CategoryID INT AUTO_INCREMENT PRIMARY KEY,
     CategoryName VARCHAR(50) NOT NULL
 );
+
+INSERT IGNORE INTO Category (CategoryID, CategoryName) VALUES 
+        (1, 'Appetizers'),
+        (2, 'Main Courses'),
+        (3, 'Desserts');
+
+
+INSERT IGNORE INTO Food (FoodID, FoodName, Price, Availability, FoodPic, Description, CategoryID) VALUES 
+        (1, 'Burger', 9.50, 'Available', LOAD_FILE('/Applications/XAMPP/xamppfiles/htdocs/restaurant-site/default images/burger.jpg'), 'Fried with huge buns', 2),
+        (2, 'Pasta', 12.50, 'Available', LOAD_FILE('/Applications/XAMPP/xamppfiles/htdocs/restaurant-site/default images/pasta.jpg'), 'Long sticks with some sticky sauce', 2),
+        (3, 'Milkshake', 10.70, 'Available', LOAD_FILE('/Applications/XAMPP/xamppfiles/htdocs/restaurant-site/default images/milkshake.jpg'), 'Fluffy and creamy', 3),
+        (4, 'Tarts', 4.00, 'Available', LOAD_FILE('/Applications/XAMPP/xamppfiles/htdocs/restaurant-site/default images/tarts.jpg'), 'Green and mean', 1),
+        (5, 'Platter', 21.80, 'Available', LOAD_FILE('/Applications/XAMPP/xamppfiles/htdocs/restaurant-site/default images/platter.jpg'), 'Spoilt with choice', 1),
+        (6, 'Choco Volcano', 7.30, 'Available', LOAD_FILE('/Applications/XAMPP/xamppfiles/htdocs/restaurant-site/default images/choco volcano.jpg'), 'Erupting with delight', 3);
 
 
 CREATE TABLE IF NOT EXISTS FoodOrder( 
