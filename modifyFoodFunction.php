f2cd5c25b0d5ddfbcfb310e588f30001c2829eff
Floopert <ngwengjie@hotmail.com>
2024-04-26 22:45:44 +0800

    ADDED MORE FORM VALIDATION FOR modifyFoodFunction & modifyFoodPage ADDED COMMENTS

--- modifyFoodFunction.php
+++ modifyFoodFunction.php
@@ -1,5 +1,5 @@
 <?php
-
+    //defining constants for form fields from modifyFoodPage.php
     define("FOOD_ID_FIELD", "foodID");
     define("FOOD_NAME_FIELD", "foodName");
     define("FOOD_CATEGORY_FIELD", "foodCategory");
@@ -8,11 +8,12 @@
     define("FOOD_DESCRIPTION_FIELD", "foodDescription");
     define("FOOD_PIC_FIELD", "foodPic");
 
-
+    //load up session variables to be used globally
     session_start();
 
     $_SESSION["foodModifyError"] = "None";
 
+    //declaring variables for the input fields from modifyFoodPage.php
     $formFoodID = $_POST[FOOD_ID_FIELD];
     $formFoodName = $_POST[FOOD_NAME_FIELD];
     $formFoodCategory = $_POST[FOOD_CATEGORY_FIELD];
@@ -20,14 +21,19 @@
     $formFoodAvailability = $_POST[FOOD_AVAILABILITY_FIELD];
     $formFoodDescription = $_POST[FOOD_DESCRIPTION_FIELD];
     $formFoodPicType = $_FILES[FOOD_PIC_FIELD]['type'];
+    //variable to determine if the food pic is to be replaced
     $replacePic = 0;
 
+    //if a new image was uploaded to the form
     if(!($_FILES[FOOD_PIC_FIELD]['size'] == 0)){
         
+        //check if uploaded image was jpeg, if yes, store contents of uploaded image in formFoodPic
+        //set replacePic to 1 to indicate that the image is to be replaced
         if($formFoodPicType == "image/jpeg"){
             $formFoodPic = file_get_contents($_FILES[FOOD_PIC_FIELD]['tmp_name']);
             $replacePic = 1;
         }
+        //if not jpeg, set 'foodModifyError' session variable and redirect to modifyFoodPage.php
         else{
             $_SESSION["foodModifyError"] = "imageTypeError";
             header("Location: modifyFoodPage.php?FoodID=" . $formFoodID);
@@ -36,19 +42,36 @@
 
 
 
-    if($_SESSION["foodModifyError"] == "None"){
+    //if the price input is not numeric, set the 'foodModifyError' session variable and redirect to modifyFoodPage.php
+    if(!is_numeric($formFoodPrice)){
+        $_SESSION["foodModifyError"] = "priceError";
+        header("Location: modifyFoodPage.php?FoodID=" . $formFoodID);
+    }
 
+    //if any fields are empty, set the 'foodModifyError' session variable and redirect to modifyFoodPage.php
+    else if($formFoodName == "" || $formFoodDescription == "" || $formFoodPrice == ""){
+        $_SESSION["foodModifyError"] = "emptyFields";
+        header("Location: modifyFoodPage.php?FoodID=" . $formFoodID);
+    }
+    
+    //if validation passed with no errors, update the food item in the database
+    else if($_SESSION["foodModifyError"] == "None"){
+
+        //declaring variables for the database connection
         $servername = "localhost";
         $username = "root";
         $password = "";
         $dbname = "restaurantDB";
 
+        //connecting to the sql database
         $conn = new mysqli($servername, $username, $password, $dbname);
 
+        //if connection fails, display error message
         if($conn -> connect_error){
             die("Connection failed: ". $conn->connect_error);
         }
 
+        //if the image is to be replaced, update the Food table with new input along with new image
         if($replacePic){
             $stmt = $conn->prepare("UPDATE Food SET FoodName = '$formFoodName', Price = '$formFoodPrice', 
                                         Availability = '$formFoodAvailability', FoodPic = ?, Description = '$formFoodDescription', 
@@ -59,6 +82,8 @@
             $stmt -> execute();
 
         }
+
+        //else, just update the Food table with new input and keep the old image
         else{
             $conn -> query("UPDATE Food SET FoodName = '$formFoodName', Price = '$formFoodPrice', 
                                 Availability = '$formFoodAvailability', Description = '$formFoodDescription', 
@@ -71,8 +96,10 @@
             $conn -> query("DELETE FROM Cart WHERE FoodID = '$formFoodID'");
         }
         
+        //close the database connection
         $conn -> close();
 
+        //redirect to modifyFoodPage.php
         header("Location: modifyFoodPage.php?FoodID=" . $formFoodID);
     }
 
--- modifyFoodPage.php
+++ modifyFoodPage.php
@@ -43,71 +43,72 @@
             //execute sql query statement to fetch all the categories labels
             $catResult = $conn -> query("SELECT * FROM Category");
 
-            //display page header title
-            echo "Modify Food: " . $row["FoodName"];
         ?>
 
-<div class="modifyFoodTitle">Modify Food: <?= $row["FoodName"] ?></div>
+        <!--page header title-->
+        <div class="modifyFoodTitle">Modify Food: <?= $row["FoodName"] ?></div>
 
             <!--form to modify the food item details-->
             <!--form will be populated with existing data of the selected food item upon loading the page-->
-<form class="modifyFoodForm" name="modifyFoodForm" action="modifyFoodFunction.php" method="post" enctype="multipart/form-data">
+            <form class="modifyFoodForm" name="modifyFoodForm" action="modifyFoodFunction.php" method="post" enctype="multipart/form-data">
 
-    <input type="hidden" id="foodID" name="foodID" value="<?= $row['FoodID']?>">
-            <!--form field for food name-->
-                    <div class="formField foodNameField">
-                        <label class="formLabel foodNameLabel" for="foodName">Food Name</label>
-                        <input class="formInput foodNameInput" type="text" id="foodName" name="foodName" value="<?= $row['FoodName']?>"> 
-                    </div>
                     <!--hidden input field to store the FoodID of the selected food item-->
                     <!--to be passed to next page for validation-->
                     <div>
                         <input type="hidden" id="foodID" name="foodID" value="<?= $row['FoodID']?>">
                     </div>
 
-                   
+                    <!--form field for food name-->
+                    <div class="formField foodNameField">
+                        <label class="formLabel foodNameLabel" for="foodName">Food Name</label>
+                        <input class="formInput foodNameInput" type="text" id="foodName" name="foodName" value="<?= $row['FoodName']?>"> 
+                    </div>
 
-                    <!--form field for food category-->    <div class="formField foodCategoryField">
-        <label class="formLabel foodCategoryLabel" for="foodCategory">Food Category</label>
-        <select class="formInput foodCategoryInput" name="foodCategory">
-                            <?php
-                                while($catRow = $catResult -> fetch_assoc()){
-                                    //check if the category is the same as the selected food item's category
-                                    //if same, set the option as default selected
-                                    if($catRow["CategoryID"] == $row["CategoryID"]){
-                                        echo "<option value='" . $catRow["CategoryID"] . "' selected>" . $catRow["CategoryName"] . "</option>";
+
+                    <!--form field for food category-->    
+                    <div class="formField foodCategoryField">
+                        <label class="formLabel foodCategoryLabel" for="foodCategory">Food Category</label>
+                        <select class="formInput foodCategoryInput" name="foodCategory">
+                                <?php
+                                    while($catRow = $catResult -> fetch_assoc()){
+                                        //check if the category is the same as the selected food item's category
+                                        //if same, set the option as default selected
+                                        if($catRow["CategoryID"] == $row["CategoryID"]){
+                                            echo "<option value='" . $catRow["CategoryID"] . "' selected>" . $catRow["CategoryName"] . "</option>";
+                                        }
+                                        //if not, display the category as an option
+                                        else{
+                                            echo "<option value='" . $catRow["CategoryID"] . "'>" . $catRow["CategoryName"] . "</option>";
+                                        }
                                     }
-                                    //if not, display the category as an option
-                                    else{
-                                        echo "<option value='" . $catRow["CategoryID"] . "'>" . $catRow["CategoryName"] . "</option>";
-                                    }
-                                }
-                            ?>
+                                ?>
                         </select>
                     </div>
-                    
+                        
                     <!--form field for food price-->
                     <div class="formField foodPriceField">
-            <label class="formLabel foodPriceLabel" for="foodPrice">Price</label>
-            <input class="formInput foodPriceInput" type="number" step="0.01" name="foodPrice" value="<?= $row['Price'] ?>">
-        </div>
+                        <label class="formLabel foodPriceLabel" for="foodPrice">Price</label>
+                        <input class="formInput foodPriceInput" type="number" step="0.01" min="0.01" name="foodPrice" value="<?= $row['Price'] ?>">
+                    </div>
 
-                    <!--form field for food availability-->        <div class="formField foodAvailabilityField">
-            <label class="formLabel foodAvailabilityLabel" for="foodAvailability">Availability</label>
-            <select class="formInput foodAvailabilityInput" name="foodAvailability">
-                            <?php
-                                //check if the food item is currently set as available or not
-                                //if available, set 'Available' as default selected
-                                if($row['Availability'] == 'Available'){
-                                    echo "<option value='Available' selected>Available</option>";
-                                    echo "<option value='Not Available'>Unavailable</option>";
-                                }
-                                //if not available, set 'Not Available' as default selected
-                                else{
-                                    echo "<option value='Available'>Available</option>";
-                                    echo "<option value='Not Available' selected>Unavailable</option>";
-                                }
-                            ?>
+
+                    <!--form field for food availability-->
+                    <div class="formField foodAvailabilityField">
+                        <label class="formLabel foodAvailabilityLabel" for="foodAvailability">Availability</label>
+                        <select class="formInput foodAvailabilityInput" name="foodAvailability">
+                                <?php
+                                    //check if the food item is currently set as available or not
+                                    //if available, set 'Available' as default selected
+                                    if($row['Availability'] == 'Available'){
+                                        echo "<option value='Available' selected>Available</option>";
+                                        echo "<option value='Not Available'>Unavailable</option>";
+                                    }
+                                    //if not available, set 'Not Available' as default selected
+                                    else{
+                                        echo "<option value='Available'>Available</option>";
+                                        echo "<option value='Not Available' selected>Unavailable</option>";
+                                    }
+                                ?>
                         </select>
                     </div>
 
@@ -122,6 +123,7 @@
                         <h4>Current Pic</h4>
                         <?= "<img class='currentPicImage' src='data:image/jpeg;base64,".base64_encode($row['FoodPic'])."' width='100' height ='100'/>"; ?>
                     </div>
+
 
                     <!--form field to replace the food picture-->
                     <div class="formField foodPicField">
@@ -146,29 +148,43 @@
                         
                     </div>
 
+                    <!--form button to go back to the previous page-->
                     <div class="formBackButton">
-                        <a class="formButton goBackButton" href='addFoodPage.php'>Go Back</a>
+                            <a class="formButton goBackButton" href='addFoodPage.php'>Go Back</a>
                     </div>
+                    
             </form>
+
+            
 
             <div class="foodModifyError">
 
 
-            <?php
-                //display error message if a replacement image was uploaded but is not in jpeg
-                if($_SESSION["foodModifyError"] == 'imageTypeError'){
-                    echo "<h3 style='color:red'>Only JPEG image is accepted</h3>";
-                    $_SESSION["foodModifyError"] = 'null';
-                }
+                <?php
+                    //display error message if a replacement image was uploaded but is not in jpeg
+                    if($_SESSION["foodModifyError"] == 'imageTypeError'){
+                        echo "<h3 style='color:red'>Only JPEG image is accepted</h3>";
+                        $_SESSION["foodModifyError"] = 'null';
+                    }
 
-                //display success message if the food item was modified successfully
-                else if($_SESSION["foodModifyError"] == 'None'){
-                    echo "<script>alert('Food modified successfully');</script>";
-                    $_SESSION["foodModifyError"] = 'null';
-                }
-            ?>                 
+                    else if($_SESSION["foodModifyError"] == 'priceError'){
+                        echo "<h3 style='color:red'>Invalid price input</h3>";
+                        $_SESSION["foodModifyError"] = 'null';
+                    }
 
-        </div>
+                    else if($_SESSION["foodModifyError"] == 'emptyFields'){
+                        echo "<h3 style='color:red'>Please do not leave any fields empty</h3>";
+                        $_SESSION["foodModifyError"] = 'null';
+                    }
+
+                    //display success message if the food item was modified successfully
+                    else if($_SESSION["foodModifyError"] == 'None'){
+                        echo "<script>alert('Food modified successfully');</script>";
+                        $_SESSION["foodModifyError"] = 'null';
+                    }
+                ?>                 
+
+            </div>
 
 
 
